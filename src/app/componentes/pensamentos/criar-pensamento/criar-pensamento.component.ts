d262263f157b5166d7b7a1c67d7314f75bab9e83
Rodrigo Neves <{ID}+{username}@users.noreply.github.com>
2023-08-15 08:00:29 -0300

    feat(editar-pensamento): added validators at editar pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,6 +3,7 @@ import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidator';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -29,7 +30,8 @@ export class CriarPensamentoComponent implements OnInit {
       ])],
       autoria: ['', Validators.compose(
         [Validators.required,
-          Validators.minLength(3)
+          Validators.minLength(3),
+          minusculoValidator
         ])],
       modelo: ['modelo1']
     })
