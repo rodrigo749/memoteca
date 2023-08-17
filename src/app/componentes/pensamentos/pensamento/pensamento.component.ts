0ccadb45ff69ceb6de700fc6d2f53a06e4b62e63
Rodrigo Neves <{ID}+{username}@users.noreply.github.com>
2023-08-17 09:21:00 -0300

    feat(favorite-icon): added service to change state false and true

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -8,32 +9,39 @@ import { Pensamento } from '../pensamento';
 })
 export class PensamentoComponent implements OnInit {
 
-@Input() pensamento: Pensamento = {
-  id: 0,
-  conteudo: 'I love angular',
-  autoria: 'Rodrigo',
-  modelo: 'modelo3',
-  favorito: false
-}
-
-constructor() {}
-
-ngOnInit(): void {
-
-}
-
-larguraPensamento(): string {
-  if(this.pensamento.conteudo.length >= 256) {
-    return 'pensamento-g'
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love angular',
+    autoria: 'Rodrigo',
+    modelo: 'modelo3',
+    favorito: false
   }
-  return 'pensamento-p'
-}
 
-mudarIconeFavorito(): string {
-  if(this.pensamento.favorito == false) {
-    return 'inativo'
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+
   }
-  return 'ativo'
-}
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string {
+    if (this.pensamento.favorito == false) {
+      return 'inativo'
+    }
+    return 'ativo'
+  }
+
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe();
+    console.log(this.pensamento.favorito);
+
+  }
 
 }
